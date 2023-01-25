fbfdf20d09ba3f43e3112624d215ce6c596b716d
aysuays <vural.aysu@gmail.com>
2023-01-25 10:09:34 +0300

    first

---
+++ src/main/java/com/example/bulk/demo/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.bulk.demo.repository;
+
+import com.example.bulk.demo.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+
+}
